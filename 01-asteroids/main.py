a76001b7f36a355e1f5a7aea0e2049a9c2f350c5
Bryan Chavajay <bchavajaydev@gmail.com>
2025-07-08 23:10:07 -0600

    Movimientos basicos

--- 01-asteroids/main.py
+++ 01-asteroids/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Configuraciones de pygame
 pygame.init()  # Inicializamos pygame
@@ -7,6 +8,11 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Creamos la pa
 clock = pygame.time.Clock()  # Reloj interno
 running = True
 
+# Fuente para dibujar los componentes
+font = pygame.font.SysFont("Couries", 40)
+
+x, y, velocity = 400, 400, 10
+
 # Bucle del juego
 while running:
     # Pull de eventos
@@ -14,9 +20,20 @@ while running:
         if event.type == pygame.QUIT:  # Evento para cerrar la ventana
             running = False
 
-    screen.fill((255, 255, 255))  # Color de la pantalla
+    screen.fill((0, 0, 0))  # Color de la pantalla
 
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and x > 0:
+        x -= velocity
+    if keys[pygame.K_RIGHT] and x < SCREEN_WIDTH - 40:
+        x += velocity
+    if keys[pygame.K_UP] and y > 0:
+        y -= velocity
+    if keys[pygame.K_DOWN] and y < SCREEN_HEIGHT - 40:
+        y += velocity
+
+    spaceship = font.render("-^-", True, (255, 255, 255))
+    screen.blit(spaceship, (x, y))
 
     pygame.display.flip()  # Actualizamos la pantalla
     clock.tick(60)  # Limitamos a 60 FPS
