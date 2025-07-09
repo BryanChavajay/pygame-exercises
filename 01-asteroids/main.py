d24915004ca3d5dd67c94bdadfc671e62fb53a86
Bryan Chavajay <bchavajaydev@gmail.com>
2025-07-08 22:49:02 -0600

    Comentarios

--- 01-asteroids/main.py
+++ 01-asteroids/main.py
@@ -1,23 +1,24 @@
 import pygame
 
-pygame.init()
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+# Configuraciones de pygame
+pygame.init()  # Inicializamos pygame
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 800  # Medidas de la pantalla
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Creamos la pantalla
+clock = pygame.time.Clock()  # Reloj interno
 running = True
 
+# Bucle del juego
 while running:
+    # Pull de eventos
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:  # Evento para cerrar la ventana
             running = False
 
-    screen.fill((255, 255, 255))
+    screen.fill((255, 255, 255))  # Color de la pantalla
 
     pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
 
-    pygame.display.flip()
+    pygame.display.flip()  # Actualizamos la pantalla
+    clock.tick(60)  # Limitamos a 60 FPS
 
 pygame.quit()
