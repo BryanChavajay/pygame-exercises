b220150640c7016b34d6ec3a5dd713d819662593
Bryan Chavajay <bchavajaydev@gmail.com>
2025-07-09 22:52:21 -0600

    Balas añadidas

--- 01-asteroids/main.py
+++ 01-asteroids/main.py
@@ -8,10 +8,19 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Creamos la pa
 clock = pygame.time.Clock()  # Reloj interno
 running = True
 
-# Fuente para dibujar los componentes
-font = pygame.font.SysFont("Couries", 40)
+x, y, velocity, git_box = 400, 400, 10, 40
 
-x, y, velocity = 400, 400, 10
+# Fuente para dibujar los componentes
+font = pygame.font.SysFont("Couries", git_box)
+spaceship = font.render("-^-", True, (255, 255, 255))
+bullet = font.render("!", True, (255, 255, 255))
+
+
+def generate_bullet(x: float, y: float):
+    return (bullet, (x, y))
+
+
+bullets = []
 
 # Bucle del juego
 while running:
@@ -20,22 +29,36 @@ while running:
         if event.type == pygame.QUIT:  # Evento para cerrar la ventana
             running = False
 
-    screen.fill((0, 0, 0))  # Color de la pantalla
+        if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+            bullets.append(generate_bullet(x + (git_box / 4), y - git_box))
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and x > 0:
         x -= velocity
-    if keys[pygame.K_RIGHT] and x < SCREEN_WIDTH - 40:
+    if keys[pygame.K_RIGHT] and x < SCREEN_WIDTH - git_box:
         x += velocity
     if keys[pygame.K_UP] and y > 0:
         y -= velocity
-    if keys[pygame.K_DOWN] and y < SCREEN_HEIGHT - 40:
+    if keys[pygame.K_DOWN] and y < SCREEN_HEIGHT - git_box:
         y += velocity
 
-    spaceship = font.render("-^-", True, (255, 255, 255))
-    screen.blit(spaceship, (x, y))
+    if len(bullets) > 0:
+        for i in range(0, len(bullets)):
+            surface, position = bullets[i]
+            if position[1] - velocity < 0:
+                bullets.pop(i)
+            else:
+                bullets[i] = (
+                    surface,
+                    (position[0], (position[1] - velocity)),
+                )
+
+    screen.fill((0, 0, 0))  # Color de la pantalla
+
+    screen.blit(spaceship, (x, y))  # Renderizamos la nave espacial
+    screen.blits(bullets)  # Renderizamos las balas
 
     pygame.display.flip()  # Actualizamos la pantalla
-    clock.tick(60)  # Limitamos a 60 FPS
+    clock.tick(30)  # Limitamos a 60 FPS
 
 pygame.quit()
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from 09-pygame-exercises!")
-
-
-if __name__ == "__main__":
-    main()
