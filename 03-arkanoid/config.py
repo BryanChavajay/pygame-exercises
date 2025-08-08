b1bc92c82018fbf6bccf5929b25e0f3cd0c1fa7d
Bryan Chavajay <bchavajaydev@gmail.com>
2025-08-08 16:18:35 -0600

    03 - Refactor asset paths to use a centralized ASSETS_PATH variable

--- 03-arkanoid/config.py
+++ 03-arkanoid/config.py
@@ -7,9 +7,16 @@ FONT_HIT_BOX = 40
 VELOCITY = 10
 FPS = 30
 
+ASSETS_PATH = os.path.join(os.getcwd(), "03-arkanoid", "assets")
+
 # Paths
 paths = {
-    "background": os.path.join(os.getcwd(), "03-arkanoid", "assets", "background.png"),
-    "starbase": os.path.join(os.getcwd(), "03-arkanoid", "assets", "starbase.png"),
-    "ball": os.path.join(os.getcwd(), "03-arkanoid", "assets", "ball.png"),
+    "background": os.path.join(ASSETS_PATH, "background.png"),
+    "starbase": os.path.join(ASSETS_PATH, "starbase.png"),
+    "ball": os.path.join(ASSETS_PATH, "ball.png"),
+    "blue_brick": os.path.join(ASSETS_PATH, "blue_brick.png"),
+    "green_brick": os.path.join(ASSETS_PATH, "green_brick.png"),
+    "orange_brick": os.path.join(ASSETS_PATH, "orange_brick.png"),
+    "pink_brick": os.path.join(ASSETS_PATH, "pink_brick.png"),
+    "red_brick": os.path.join(ASSETS_PATH, "red_brick.png"),
 }
